9e49fd7abb3d2c054fb2dff81ed55cd76887e965
weliasz <weliasz@splunk.com>
2021-02-17 16:54:32 +0100

    feat: refactoring to class and added test skeleton

--- splunk_connect_for_snmp_poller/mongo.py
+++ splunk_connect_for_snmp_poller/mongo.py
@@ -1,12 +1,17 @@
 from pymongo import MongoClient
 
-client = MongoClient(port=27017)
-walked_hosts = client.snmp_poller.snmp_walk
 
+class WalkedHostsRepository:
+    def __init__(self, mongo_config):
+        self._client = MongoClient(host=mongo_config['host'], port=mongo_config['port'])
+        self._walked_hosts = self._client[mongo_config['database']][mongo_config['collection']]
 
-def contains_host(host):
-    return walked_hosts.find({'_id': host}).count()
+    def contains_host(self, host):
+        return self._walked_hosts.find({'_id': host}).count()
 
+    def add_host(self, host):
+        self._walked_hosts.insert_one({'_id': host})
 
-def add_host(host):
-    walked_hosts.insert_one({'_id': host})
+    def clear(self):
+        self._walked_hosts.remove()
+
---
+++ tests/mongo_integration_test.py
@@ -0,0 +1,17 @@
+from splunk_connect_for_snmp_poller.mongo import WalkedHostsRepository
+
+#TODO as for now this is just a demo on how to use WalkedHostsRepository
+#TODO final version should use fixures to run MongoDB in Docker
+def main():
+    mongo_config = {'host': 'localhost', 'port': 27017, 'database': 'snmp_poller', 'collection': 'walked_hosts'}
+
+    mongo = WalkedHostsRepository(mongo_config)
+
+    mongo.clear()
+    mongo.add_host('192.168.0.2')
+    print(mongo.contains_host('192.168.0.2'))
+
+
+
+if __name__ == '__main__':
+    main()
