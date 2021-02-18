944bf0cf6b13226edf78c3ed419655f651c471ef
weliasz <77732905+weliasz@users.noreply.github.com>
2021-02-18 18:16:00 +0100

    feat: mongo DB API for persistent set (#15)
    * feat: mongo DB API for persistent set
    
    * feat: refactoring to class and added test skeleton
    
    * feat: added method for deleting hosts

---
+++ splunk_connect_for_snmp_poller/mongo.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+
+class WalkedHostsRepository:
+    def __init__(self, mongo_config):
+        self._client = MongoClient(host=mongo_config['host'], port=mongo_config['port'])
+        self._walked_hosts = self._client[mongo_config['database']][mongo_config['collection']]
+
+    def contains_host(self, host):
+        return self._walked_hosts.find({'_id': host}).count()
+
+    def add_host(self, host):
+        self._walked_hosts.insert_one({'_id': host})
+
+    def delete_host(self, host):
+        self._walked_hosts.delete_many({'_id': host})
+
+    def clear(self):
+        self._walked_hosts.remove()
+
