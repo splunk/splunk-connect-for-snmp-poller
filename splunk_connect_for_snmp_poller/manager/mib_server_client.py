d12be2949501b0af7b8b63092e7c1d7a2f2ae066
Mayur Pipaliya <mpipaliya@splunk.com>
2021-03-03 10:28:16 -0800

    fix: exception handling when mib server is unreachable

--- splunk_connect_for_snmp_poller/manager/mib_server_client.py
+++ splunk_connect_for_snmp_poller/manager/mib_server_client.py
@@ -6,6 +6,14 @@ import os
 
 logger = logging.getLogger(__name__)
 
+class SharedException(Exception):
+    """Raised when the input value is too large"""
+    def __init__(self, msg='Default Shared Exception occured.', *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
+    
+    pass
+
+
 def get_translation(var_binds, mib_server_url):
     """
     @param var_binds: var_binds object getting from SNMP agents
@@ -39,7 +47,14 @@ def get_translation(var_binds, mib_server_url):
         resp = requests.request("POST", TRANSLATION_URL, headers=headers, data=payload)
     except Exception as e:
         logger.error(f"MIB server is unreachable! Error happened while communicating to MIB server to perform the Translation: {e}")
+        raise SharedException("MIB server is unreachable!")
+
+
     if resp.status_code != 200:
-        logger.error(f"[-] Mib Server API Error with code: {resp.status_code}")
+        logger.error(f"[-] MIB Server API Error with code: {resp.status_code}")
+        raise SharedException(f"MIB Server API Error with code: {resp.status_code")
+
+    # *TODO*: For future release could retain failed translations in some place to re-translate.
+
     trap_event_string = resp.text
     return trap_event_string
