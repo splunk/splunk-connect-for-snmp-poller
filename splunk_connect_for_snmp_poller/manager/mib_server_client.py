ff9bd5985af08ccc5873b81e8a776fe339ff8065
Mayur Pipaliya <mpipaliya@splunk.com>
2021-03-02 18:05:26 -0800

    feat: mib server client from trap

--- splunk_connect_for_snmp_poller/manager/mib_server_client.py
+++ splunk_connect_for_snmp_poller/manager/mib_server_client.py
@@ -3,6 +3,7 @@ import json
 import requests
 import os
 
+
 logger = logging.getLogger(__name__)
 
 def get_translation(var_binds, mib_server_url):
@@ -14,11 +15,15 @@ def get_translation(var_binds, mib_server_url):
     # Construct the payload
     payload = {}
     var_binds_list = []
+    # *TODO*: Below differs a bit between poller and trap!
+
     for name, val in var_binds:
         var_bind = {
-            "oid": name.prettyPrint(),
+            # "oid": name.prettyPrint(),
+            "oid": str(name),
             "oid_type": name.__class__.__name__,
-            "val": val.prettyPrint(),
+            # "val": val.prettyPrint(),
+            "val": str(val),
             "val_type": val.__class__.__name__
         }
         var_binds_list.append(var_bind)
@@ -30,13 +35,11 @@ def get_translation(var_binds, mib_server_url):
     endpoint = "translation"
     TRANSLATION_URL = os.path.join(mib_server_url.strip('/'), endpoint)
     logger.debug(f"[-] TRANSLATION_URL: {TRANSLATION_URL}")
-
     try:
         resp = requests.request("POST", TRANSLATION_URL, headers=headers, data=payload)
     except Exception as e:
-        logger.error(f"Error happened during talk to MIB server to do the Translation: {e}")
+        logger.error(f"MIB server is unreachable! Error happened while communicating to MIB server to perform the Translation: {e}")
     if resp.status_code != 200:
         logger.error(f"[-] Mib Server API Error with code: {resp.status_code}")
     trap_event_string = resp.text
     return trap_event_string
-    
