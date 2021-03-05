dedf8f50c80d6b316d015921cd89e78b032bc328
Yuan Ling <yling@splunk.com>
2021-03-04 17:10:45 -0800

    feat: Add SNMP Walk functionality & Use HEC to integrate with Splunk

--- splunk_connect_for_snmp_poller/manager/tasks.py
+++ splunk_connect_for_snmp_poller/manager/tasks.py
@@ -3,48 +3,133 @@ logger = logging.getLogger(__name__)
 
 from splunk_connect_for_snmp_poller.manager.celery_client import app
 from splunk_connect_for_snmp_poller.manager.mib_server_client import get_translation
+from splunk_connect_for_snmp_poller.manager.hec_config import HecConfiguration
+from splunk_connect_for_snmp_poller.manager.hec_sender import post_data_to_splunk_hec
 from pysnmp.hlapi import *
+import json
 
 
 @app.task
-def snmp_get(host, version, community, profile, mib_server_url):
-    
+def snmp_get(host, port, version, community, profile, mib_server_url, index):
+    hec_config = HecConfiguration()
+    results = []
+    #nextCmd - snmpwalk
+    if profile[-1] == "*":
+        logger.debug(f'Executing SNMP WALK for {host} profile={profile}')
+        for (errorIndication,errorStatus,errorIndex,varBinds) in nextCmd(
+            SnmpEngine(),
+            CommunityData(community),
+            # TODO do we have port in inventory.csv
+            UdpTransportTarget((host, port)),
+            ContextData(),
+            ObjectType(ObjectIdentity(profile[:-2])),lexicographicMode=False):
+        
+            if errorIndication:
+                result = errorIndication
+                logger.debug(result)
+                results.append((result, False))
+                break
+            elif errorStatus:
+                result = '%s at %s' % (errorStatus.prettyPrint(),
+                    errorIndex and varBinds[int(errorIndex) - 1][0] or '?')
+                logger.debug(result)
+                results.append((result, False))
+                break
+            else:
+                result, metric = get_var_binds_string(mib_server_url, hec_config, varBinds)    
+                results.append((result, metric))
     # getCmd - snmpget
-    logger.debug(f'Executing SNMP GET for {host} version={version}')
+    else:       
+        logger.debug(f'Executing SNMP GET for {host} profile={profile}')       
+        # check if it's in mongo
+        errorIndication, errorStatus, errorIndex, varBinds = next(
+        getCmd(SnmpEngine(),
+            CommunityData(community, mpModel=0),
+            # TODO do we have port in inventory.csv
+            UdpTransportTarget((host, port)),
+            ContextData(),
+            ObjectType(ObjectIdentity(profile)))
+        )
+        if errorIndication:
+            result = errorIndication
+            metric = False
+            logger.error(result)
+        elif errorStatus:
+            result = '%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?')
+            metric = False
+            logger.error(result)
+        else:
+            result, metric = get_var_binds_string(mib_server_url, hec_config, varBinds)
+        results.append((result,metric))
+       
     
-    # check if it's in mongo
-    errorIndication, errorStatus, errorIndex, varBinds = next(
-    getCmd(SnmpEngine(),
-        CommunityData(community, mpModel=0),
-        UdpTransportTarget((host, 1161)),
-        ContextData(),
-        #    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)),
-        #    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysUpTime', 0)),
-        #    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.2.0')),
-        ObjectType(ObjectIdentity(profile)))
-    )
-    if errorIndication:
-        logger.error(errorIndication)
-    elif errorStatus:
-        logger.error('%s at %s' % (errorStatus.prettyPrint(),
-                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-    else:
-        logger.debug(f"varBinds: {varBinds}")
-        for varBind in varBinds:
-            logger.debug(' = '.join([x.prettyPrint() for x in varBind]))
-        
-        try:
-            for name, val in varBinds:
-                logger.debug("{} - {}".format(name, val))
-        except Exception as e:
-            logger.debug(f'Exception occured while logging varBinds name & value. Exception: {e}') 
-        
-        try:
-            result = get_translation(varBinds, mib_server_url)
-        except Exception as e:
-            logger.debug(f'Could not perform translation. Exception: {e}')         
+    logger.debug(f"***results list with {len(results)} items***\n{results}")
 
-        logger.debug("===\n\n{}\n\n===".format(result))
-    # add in mongo
-    
-    return f'Executing SNMP GET for {host} version={version}'
+    # Post mib event to splunk HEC
+    for event, metric in results:
+        post_data_to_splunk_hec(host, event, metric, index, hec_config)
+
+            
+    return f'Executing SNMP GET for {host} version={version} profile={profile}'
+
+def is_metric_data(hec_config, varBinds):
+    """
+    Check two conditions to see if the varBinds belongs to metric data. 
+    1. if metric token exists
+    2. if mib value is int/float 
+    @param hec_config: HecConfiguration Object
+    @param varBinds: varBinds Object
+    @return: boolean
+    """
+    splunk_hec_metric_token = hec_config.get_authentication_metric_token()
+    # check if the metric token exist
+    if splunk_hec_metric_token:
+        # check if the mib value is float
+        for name, value in varBinds:
+            try:
+                float(value.prettyPrint())
+                return True
+            except ValueError:
+                return False
+    else:
+        return False
+
+def get_var_binds_string(mib_server_url, hec_config, varBinds):
+    """
+    Get the translated/formatted var_binds string depending on whether the varBinds is an event or metric
+    Note: if it failed to get translation, return the the original varBinds
+    @return result: formated string ready to be sent to Splunk HEC
+    @return metric: boolean, metric data flag 
+    """
+    # check if this is metric data
+    metric = is_metric_data(hec_config, varBinds)
+    # Get Original varbinds as backup in case the mib-server is unreachable
+    try:
+        for name, val in varBinds:             
+            # Original oid
+            # TODO Discuss: should we return the original oid 
+            # if the mib server is unreachable
+            # should we format it align with the format of the translated one
+            # result = "{} = {}".format(name.prettyPrint(), val.prettyPrint())
+            if metric:
+                result = {
+                    "metric_name": name.prettyPrint(),
+                    "_value": val.prettyPrint(),
+                }
+                result = json.dumps(result)
+            else: 
+                result = "{} = {}".format(name.prettyPrint(), val.prettyPrint())
+
+    except Exception as e:
+        logger.debug(f'Exception occured while logging varBinds name & value. Exception: {e}') 
+
+    # Overrid the varBinds string with translated varBinds string  
+    try:
+        result = get_translation(varBinds, mib_server_url, metric)
+        logger.debug(f"=========result=======\n{result}")
+    except Exception as e:
+        logger.debug(f'Could not perform translation. Exception: {e}')
+    logger.debug(f"###############final result -- metric: {metric}#######################\n{result}")
+    return result, metric
+
