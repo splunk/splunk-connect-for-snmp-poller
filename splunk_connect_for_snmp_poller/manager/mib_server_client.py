9b77d9fe38e012ed238140453332b394a1ffa5cc
Yuan Ling <yling@splunk.com>
2021-03-05 16:30:31 -0800

    fix: add retry mechanism when communicating with mib server

--- splunk_connect_for_snmp_poller/manager/mib_server_client.py
+++ splunk_connect_for_snmp_poller/manager/mib_server_client.py
@@ -2,6 +2,8 @@ import logging
 import json
 import requests
 import os
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
 logger = logging.getLogger(__name__)
@@ -48,7 +50,21 @@ def get_translation(var_binds, mib_server_url, metric=False):
     }
 
     try:
-        resp = requests.request("POST", TRANSLATION_URL, headers=headers, data=payload, params=params)
+        # resp = requests.request("POST", TRANSLATION_URL, headers=headers, data=payload, params=params)
+
+        # use Session with Retry
+        retry_strategy = Retry(
+            total=3,
+            backoff_factor = 1,
+            status_forcelist=[429, 500, 502, 503, 504],
+            method_whitelist=["GET", "POST"]
+        )
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        session = requests.Session()
+        session.mount("https://", adapter)
+        session.mount("http://", adapter)
+        resp = session.post(TRANSLATION_URL, headers=headers, data=payload, params=params)
+
     except Exception as e:
         logger.error(f"MIB server is unreachable! Error happened while communicating to MIB server to perform the Translation: {e}")
         raise SharedException("MIB server is unreachable!")
