f8d2c7c7a4d753c1ddb823dbc1209bb1918d900e
Pawel Szkamruk <pszkamruk@splunk.com>
2021-03-31 14:47:00 +0200

    feat: feat: added job which checks if code is formatted correctly in CircleCi, code formatted using black

--- tests/snmp_utils/fixtures/snmp_simulator_docker_setup.py
+++ tests/snmp_utils/fixtures/snmp_simulator_docker_setup.py
@@ -7,19 +7,24 @@ logger = logging.getLogger(__name__)
 
 def is_snmp_simulator_running(snmp_config):
     import pysnmp.hlapi as snmp
-    iterator = snmp.getCmd(snmp.SnmpEngine(), snmp.CommunityData('public'),
-                           snmp.UdpTransportTarget((snmp_config['host'], snmp_config['port'])),
-                           snmp.ContextData(),
-                           snmp.ObjectType(snmp.ObjectIdentity('1.3.6.1.2.1.1.1.0')))
+
+    iterator = snmp.getCmd(
+        snmp.SnmpEngine(),
+        snmp.CommunityData("public"),
+        snmp.UdpTransportTarget((snmp_config["host"], snmp_config["port"])),
+        snmp.ContextData(),
+        snmp.ObjectType(snmp.ObjectIdentity("1.3.6.1.2.1.1.1.0")),
+    )
     error_indication, error_status, error_index, var_binds = next(iterator)
     return True if error_indication is None else False
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def snmp_simulator_service(request, docker_services):
-    service_name = 'snmpsim'
+    service_name = "snmpsim"
     docker_services.start(service_name)
-    snmp_config = {'host': 'localhost', 'port': 161}
-    docker_services.wait_until_responsive(timeout=180.0, pause=1.0,
-                                          check=lambda: is_snmp_simulator_running(snmp_config))
-    logger.info(f'Initialized {service_name} from Docker with {snmp_config}')
+    snmp_config = {"host": "localhost", "port": 161}
+    docker_services.wait_until_responsive(
+        timeout=180.0, pause=1.0, check=lambda: is_snmp_simulator_running(snmp_config)
+    )
+    logger.info(f"Initialized {service_name} from Docker with {snmp_config}")
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,6 +3,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def test_splunk_integration(splunk_service, mongo_service, snmp_simulator_service, rabbitmq_service):
-    logger.info('Running test_splunk_integration()')
+def test_splunk_integration(
+    splunk_service, mongo_service, snmp_simulator_service, rabbitmq_service
+):
+    logger.info("Running test_splunk_integration()")
     assert True
