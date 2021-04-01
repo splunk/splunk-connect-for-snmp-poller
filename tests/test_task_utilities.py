dc3da82cbba5d8ca6c1524a68279933da8acff36
weliasz <weliasz@splunk.com>
2021-04-01 10:35:50 +0200

    feat: reformatting

--- tests/test_task_utilities.py
+++ tests/test_task_utilities.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
 
-from splunk_connect_for_snmp_poller.manager.task_utilities import is_metric_data, parse_port
+from splunk_connect_for_snmp_poller.manager.task_utilities import (
+    is_metric_data,
+    parse_port,
+)
 
 
 class ObjectTypeMock:
-
     def __init__(self, value):
         self._value = value
 
@@ -14,30 +16,29 @@ class ObjectTypeMock:
 
 class TestTaskUtilities(TestCase):
     def test_metric_for_integer(self):
-        self.assertTrue(is_metric_data([('some_name', ObjectTypeMock('1'))]))
+        self.assertTrue(is_metric_data([("some_name", ObjectTypeMock("1"))]))
 
     def test_metric_for_negative_integer(self):
-        self.assertTrue(is_metric_data([('some_name', ObjectTypeMock('-5'))]))
+        self.assertTrue(is_metric_data([("some_name", ObjectTypeMock("-5"))]))
 
     def test_metric_for_float(self):
-        self.assertTrue(is_metric_data([('some_name', ObjectTypeMock('2.0'))]))
+        self.assertTrue(is_metric_data([("some_name", ObjectTypeMock("2.0"))]))
 
     def test_metric_for_negative_float(self):
-        self.assertTrue(is_metric_data([('some_name', ObjectTypeMock('-2.0'))]))
+        self.assertTrue(is_metric_data([("some_name", ObjectTypeMock("-2.0"))]))
 
     def test_metric_for_zero(self):
-        self.assertTrue(is_metric_data([('some_name', ObjectTypeMock('0'))]))
+        self.assertTrue(is_metric_data([("some_name", ObjectTypeMock("0"))]))
 
     def test_metric_for_string(self):
-        self.assertFalse(is_metric_data([('some_name', ObjectTypeMock('asdad'))]))
+        self.assertFalse(is_metric_data([("some_name", ObjectTypeMock("asdad"))]))
 
     def test_port_parse_with_default_port(self):
-        host, port = parse_port('192.168.0.13')
-        self.assertEqual(host, '192.168.0.13')
+        host, port = parse_port("192.168.0.13")
+        self.assertEqual(host, "192.168.0.13")
         self.assertEqual(port, 161)
 
     def test_port_parse_with_specified_port(self):
-        host, port = parse_port('192.168.0.13:765')
-        self.assertEqual(host, '192.168.0.13')
-        self.assertEqual(port, '765')
-
+        host, port = parse_port("192.168.0.13:765")
+        self.assertEqual(host, "192.168.0.13")
+        self.assertEqual(port, "765")
