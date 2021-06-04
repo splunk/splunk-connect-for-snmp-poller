0225fb52e0929f0fa94b84f0821fd8bd3e2f8f78
Ryan Faircloth <35384120+rfaircloth-splunk@users.noreply.github.com>
2021-06-04 14:09:14 -0400

    Release (#68)
    * ci: Replace circleCI and add tools
    
    * ci: fix token
    
    * fix: bump version
    
    * Update .releaserc
    
    * chore(release): 1.4.4-develop.1
    
    ## [1.4.4-develop.1](https://github.com/splunk/splunk-connect-for-snmp-poller/compare/v1.4.3...v1.4.4-develop.1) (2021-06-04)
    
    ### Bug Fixes
    
    * bump version ([65b37e6](https://github.com/splunk/splunk-connect-for-snmp-poller/commit/65b37e62e66cdb69816bdca3010262ca00d45c2c))
    
    Co-authored-by: Addon Factory template <addonfactory@splunk.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- splunk_connect_for_snmp_poller/utilities.py
+++ splunk_connect_for_snmp_poller/utilities.py
@@ -1,18 +1,37 @@
-#   ########################################################################
-#   Copyright 2021 Splunk Inc.
 #
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+# Copyright 2021 Splunk Inc.
 #
-#        http://www.apache.org/licenses/LICENSE-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-#   ########################################################################
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
 import argparse
 import logging
 import signal
@@ -84,7 +103,7 @@ def parse_config_file(config_file_path):
     logger.info(f"Config file is {config_file_path}")
     try:
         with open(config_file_path, "r") as yaml_file:
-            server_config = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            server_config = yaml.safe_load(yaml_file)
         logger.debug(f"Server Config is:  {server_config}")
     except Exception as e:
         logger.debug(f"Exception occurred while loading YAML: {e}")
