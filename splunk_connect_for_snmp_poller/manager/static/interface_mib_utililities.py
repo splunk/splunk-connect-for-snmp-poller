11cc16e01126888f2feb9c6b4fe48b545add75fc
lstoppa <lstoppa@splunk.com>
2021-07-29 14:16:54 +0200

    fix: lint (ADDON-39580) - sanitizers (formatting, again)

--- splunk_connect_for_snmp_poller/manager/static/interface_mib_utililities.py
+++ splunk_connect_for_snmp_poller/manager/static/interface_mib_utililities.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-()
 import logging
 
 from splunk_connect_for_snmp_poller.manager.realtime.interface_mib import (
