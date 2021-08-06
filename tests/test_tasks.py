b547660cadadc4bdee763f713cf60f5c1f21e0c3
omrozowicz-splunk <omrozowicz@splunk.com>
2021-08-06 11:54:47 +0200

    add mock for mib_string_handler in unit tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,14 +1,11 @@
 from unittest import TestCase
+from unittest.mock import patch
+
 from pysnmp.hlapi import ObjectIdentity, ObjectType
-import collections
 
-from splunk_connect_for_snmp_poller.manager.task_utilities import (
-    VarbindCollection
-)
+from splunk_connect_for_snmp_poller.manager.task_utilities import VarbindCollection
 
-from splunk_connect_for_snmp_poller.manager.tasks import (
-    sort_varbinds
-)
+from splunk_connect_for_snmp_poller.manager.tasks import sort_varbinds
 
 
 def cast_helper(varbind):
@@ -19,29 +16,36 @@ def cast_helper(varbind):
 
 
 class TestTasks(TestCase):
-
     def test_sort_varbinds_get(self):
-        varbinds = '1.3.6.1.2.1.2.2'
+        varbinds = "1.3.6.1.2.1.2.2"
         get_varbinds_result = VarbindCollection(bulk=[], get=cast_helper(varbinds))
         actual_result = sort_varbinds([varbinds])
         self.assertEqual(str(actual_result.get), str(get_varbinds_result.get))
 
     def test_sort_varbinds_bulk(self):
-        varbinds = ['CISCO-FC-MGMT-MIB', 'cfcmPortLcStatsEntry']
+        varbinds = ["CISCO-FC-MGMT-MIB", "cfcmPortLcStatsEntry"]
         bulk_varbinds_result = VarbindCollection(bulk=cast_helper(varbinds), get=[])
-        actual_result = sort_varbinds([varbinds])
+        with patch(
+            "splunk_connect_for_snmp_poller.manager.tasks.mib_string_handler",
+            return_value=bulk_varbinds_result,
+        ):
+            actual_result = sort_varbinds([varbinds])
         self.assertEqual(str(actual_result.bulk), str(bulk_varbinds_result.bulk))
 
     def test_sort_varbinds_bulk_star(self):
-        varbinds = ['1.3.6.1.2.1.2.*']
-        bulk_varbinds_result = VarbindCollection(bulk=cast_helper('1.3.6.1.2.1.2'), get=[])
+        varbinds = ["1.3.6.1.2.1.2.*"]
+        bulk_varbinds_result = VarbindCollection(
+            bulk=cast_helper("1.3.6.1.2.1.2"), get=[]
+        )
         actual_result = sort_varbinds(varbinds)
         self.assertEqual(str(actual_result.bulk), str(bulk_varbinds_result.bulk))
 
     def test_sort_varbinds_bulk_get(self):
-        varbinds = ['1.3.6.1.2.1.2.*', '1.3.6.1.2.1.2.1']
-        varbinds_bulk = cast_helper('1.3.6.1.2.1.2')
-        varbinds_result = VarbindCollection(bulk=varbinds_bulk, get=cast_helper('1.3.6.1.2.1.2.1'))
+        varbinds = ["1.3.6.1.2.1.2.*", "1.3.6.1.2.1.2.1"]
+        varbinds_bulk = cast_helper("1.3.6.1.2.1.2")
+        varbinds_result = VarbindCollection(
+            bulk=varbinds_bulk, get=cast_helper("1.3.6.1.2.1.2.1")
+        )
         actual_result = sort_varbinds(varbinds)
         self.assertEqual(str(actual_result.bulk), str(varbinds_result.bulk))
         self.assertEqual(str(actual_result.get), str(varbinds_result.get))
