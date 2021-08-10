33acbbfb15241c481c1f4c8894f0856da070f403
omrozowicz-splunk <86965961+omrozowicz-splunk@users.noreply.github.com>
2021-08-10 11:02:01 +0200

    feat: Replace multiple SNMP walks with one SNMP Bulk  (#100)
    * feat: ADDON-37033 (implement bulk operation for non-oid profiles)
    
    * add ObjectType/ObjectIdentity casting of oid values for bulk action
    
    * delete debug lines
    
    * add info loggers
    
    * 1. remove debug logs
    2. small refactor
    3. add unit tests
    
    * change get to walk
    
    * fix walk/get mismatch
    
    * update get method
    
    * update mib_string_handler
    
    * revert commented lines
    
    * fix casting bug
    
    * fix bulk for * oids
    
    * refactor and change unit tests
    
    * update unit tests
    
    * add casting for get varbinds
    
    * code refactor with black
    
    * Update tests/test_tasks.py
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * add mock for mib_string_handler in unit tests
    
    * rename get_data, walk_handler and get_handler
    
    * add mocking for imports in unit tests
    
    * add mocking for imports in unit tests
    
    * reformat code
    
    * cleanup unit tests
    
    * reorganize imports in test file
    
    * format imports
    
    * attempt to fix linter workflow
    
    * attempt to fix linter workflow
    
    * attempt to fix linter workflow
    
    * implement _any_failures_happened function to avoid duplicates
    
    * format task_utilities file
    
    * format task_utilities file
    
    * change mib_string_handler description from snmpwalk to snmpbulkwalk
    
    * add casting for variable in a single get scenario
    
    Co-authored-by: lstoppa <77723162+lstoppa@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/test_tasks.py
@@ -0,0 +1,75 @@
+#
+# Copyright 2021 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import sys
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from pysnmp.hlapi import ObjectIdentity, ObjectType
+
+sys.modules["splunk_connect_for_snmp_poller.manager.celery_client"] = Mock()
+from splunk_connect_for_snmp_poller.manager.task_utilities import (
+    VarbindCollection,
+)
+from splunk_connect_for_snmp_poller.manager.tasks import sort_varbinds
+
+
+def cast_helper(varbind):
+    if isinstance(varbind, list):
+        return [ObjectType(ObjectIdentity(varbind[0], varbind[1]))]
+    else:
+        return [ObjectType(ObjectIdentity(varbind))]
+
+
+class TestTasks(TestCase):
+    def test_sort_varbinds_get(self):
+        varbinds = "1.3.6.1.2.1.2.2"
+        get_varbinds_result = VarbindCollection(bulk=[], get=cast_helper(varbinds))
+        actual_result = sort_varbinds([varbinds])
+        self.assertEqual(str(actual_result.get), str(get_varbinds_result.get))
+
+    def test_sort_varbinds_bulk(self):
+        varbinds = ["CISCO-FC-MGMT-MIB", "cfcmPortLcStatsEntry"]
+        bulk_varbinds_result = VarbindCollection(bulk=cast_helper(varbinds), get=[])
+        with patch(
+            "splunk_connect_for_snmp_poller.manager.tasks.mib_string_handler",
+            return_value=bulk_varbinds_result,
+        ):
+            actual_result = sort_varbinds([varbinds])
+        self.assertEqual(str(actual_result.bulk), str(bulk_varbinds_result.bulk))
+
+    def test_sort_varbinds_bulk_star(self):
+        varbinds = ["1.3.6.1.2.1.2.*"]
+        bulk_varbinds_result = VarbindCollection(
+            bulk=cast_helper("1.3.6.1.2.1.2"), get=[]
+        )
+        actual_result = sort_varbinds(varbinds)
+        self.assertEqual(str(actual_result.bulk), str(bulk_varbinds_result.bulk))
+
+    def test_sort_varbinds_bulk_get(self):
+        varbinds = ["1.3.6.1.2.1.2.*", "1.3.6.1.2.1.2.1"]
+        varbinds_bulk = cast_helper("1.3.6.1.2.1.2")
+        varbinds_result = VarbindCollection(
+            bulk=varbinds_bulk, get=cast_helper("1.3.6.1.2.1.2.1")
+        )
+        actual_result = sort_varbinds(varbinds)
+        self.assertEqual(str(actual_result.bulk), str(varbinds_result.bulk))
+        self.assertEqual(str(actual_result.get), str(varbinds_result.get))
+
+    def test_sort_varbinds_empty(self):
+        varbinds = []
+        varbinds_result = VarbindCollection(bulk=[], get=[])
+        actual_result = sort_varbinds(varbinds)
+        self.assertEqual(actual_result.__dict__, varbinds_result.__dict__)
