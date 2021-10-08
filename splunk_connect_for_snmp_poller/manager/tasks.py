62f8d34f3fbf09c348654e21d7e63d91f8e2611a
omrozowicz-splunk <omrozowicz@splunk.com>
2021-10-08 15:46:10 +0200

    fix: bring back rpevious changes

--- splunk_connect_for_snmp_poller/manager/tasks.py
+++ splunk_connect_for_snmp_poller/manager/tasks.py
@@ -212,7 +212,6 @@ async def snmp_polling_async(
                     *get_bulk_specific_parameters, *static_parameters, prepared_profile  # type: ignore
                 )
 
-        raise Exception("Random exception to test if it fixes rabbitmq")
     except Exception:
         logger.exception(
             f"Error occurred while executing SNMP polling for {host}, version={ir.version}, profile={ir.profile}"
