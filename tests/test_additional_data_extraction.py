0e756e1a0cf7463c090d8017c00ae7b5a2a2dadf
weliasz <weliasz@splunk.com>
2021-10-29 13:04:20 +0200

    fix: build fix

--- tests/test_additional_data_extraction.py
+++ tests/test_additional_data_extraction.py
@@ -77,16 +77,16 @@ class TestAdditionalDataExtraction(TestCase):
             server_config,
         )
 
-        self.assertEqual(fields['IP_one'], '192_168_0_1')
-        self.assertEqual(fields['port'], '161')
-        self.assertEqual(fields['IP_two'], '127_0_0_1')
-        self.assertEqual(fields['index_number'], '5')
+        self.assertEqual(fields["IP_one"], "192_168_0_1")
+        self.assertEqual(fields["port"], "161")
+        self.assertEqual(fields["IP_two"], "127_0_0_1")
+        self.assertEqual(fields["index_number"], "5")
 
-        self.assertEqual(fields3['protocol_version_one'], 'ipv4')
-        self.assertEqual(fields3['IP_one'], '0_0_0_0')
-        self.assertEqual(fields3['port_one'], '111')
-        self.assertEqual(fields3['protocol_version_two'], 'ipv4')
-        self.assertEqual(fields3['IP_two'], '0_0_0_0')
-        self.assertEqual(fields3['index_number'], '0')
-        self.assertEqual(fields3['port_two'], '13348')
+        self.assertEqual(fields3["protocol_version_one"], "ipv4")
+        self.assertEqual(fields3["IP_one"], "0_0_0_0")
+        self.assertEqual(fields3["port_one"], "111")
+        self.assertEqual(fields3["protocol_version_two"], "ipv4")
+        self.assertEqual(fields3["IP_two"], "0_0_0_0")
+        self.assertEqual(fields3["index_number"], "0")
+        self.assertEqual(fields3["port_two"], "13348")
 
