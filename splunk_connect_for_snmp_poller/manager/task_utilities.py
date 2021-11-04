232c21e5e18622f004094fcf80b99f7716dec8f9
weliasz <weliasz@splunk.com>
2021-11-04 11:13:44 +0100

    feat: suffix parsing

--- splunk_connect_for_snmp_poller/manager/task_utilities.py
+++ splunk_connect_for_snmp_poller/manager/task_utilities.py
@@ -229,7 +229,6 @@ async def snmp_get_handler(
     ir,
     additional_metric_fields,
     var_binds,
-    server_config,
 ):
     """
     Perform the SNMP Get for an oid,
@@ -261,7 +260,6 @@ async def snmp_get_handler(
                 index,
                 ir,
                 additional_metric_fields,
-                server_config,
                 one_time_flag=OnetimeFlag.is_a_walk(one_time_flag),
                 mib_enricher=mib_enricher,
             )
@@ -278,7 +276,6 @@ async def snmp_get_handler(
                 index,
                 ir,
                 additional_metric_fields,
-                server_config,
                 one_time_flag=OnetimeFlag.is_a_walk(one_time_flag),
                 is_error=is_error,
             )
@@ -333,7 +330,6 @@ def _any_walk_failure_happened(
     is_metric,
     ir,
     additional_metric_fields,
-    server_config,
     var_binds,
 ):
     is_error, result = prepare_error_message(
@@ -349,7 +345,6 @@ def _any_walk_failure_happened(
             index,
             ir,
             additional_metric_fields,
-            server_config,
             one_time_flag=one_time_flag,
             is_error=is_error,
         )
@@ -390,7 +385,6 @@ async def snmp_bulk_handler(
     ir,
     additional_metric_fields,
     var_binds,
-    server_config,
 ):
     """
     Perform the SNMP Bulk for an array of oids
@@ -426,7 +420,6 @@ async def snmp_bulk_handler(
                     index,
                     ir,
                     additional_metric_fields,
-                    server_config,
                     one_time_flag=OnetimeFlag.is_a_walk(one_time_flag),
                     mib_enricher=mib_enricher,
                 )
@@ -443,7 +436,6 @@ async def snmp_bulk_handler(
                     index,
                     ir,
                     additional_metric_fields,
-                    server_config,
                     one_time_flag=OnetimeFlag.is_a_walk(one_time_flag),
                     is_error=is_error,
                 )
@@ -493,14 +485,13 @@ async def walk_handler(
             is_metric,
             ir,
             additional_metric_fields,
-            server_config,
             var_binds,
         ):
             if OnetimeFlag.is_a_walk(one_time_flag):
                 error_in_one_time_walk = True
             break
         else:
-            result, is_metric = await get_translated_string(mib_server_url, var_binds)
+            result = await get_translated_string(mib_server_url, var_binds)
             post_data_to_splunk_hec(
                 hec_sender,
                 host,
@@ -509,7 +500,6 @@ async def walk_handler(
                 index,
                 ir,
                 additional_metric_fields,
-                server_config,
                 one_time_flag=OnetimeFlag.is_a_walk(one_time_flag),
             )
     if OnetimeFlag.is_a_walk(one_time_flag):
@@ -567,7 +557,6 @@ async def walk_handler_with_enricher(
     one_time_flag,
     ir,
     additional_metric_fields,
-    server_config,
 ):
     """
     Perform the SNMP Walk for oid end with *,
@@ -597,7 +586,6 @@ async def walk_handler_with_enricher(
             is_metric,
             ir,
             additional_metric_fields,
-            server_config,
             var_binds,
         ):
             break
@@ -620,7 +608,6 @@ async def walk_handler_with_enricher(
                 index,
                 ir,
                 additional_metric_fields,
-                server_config,
                 one_time_flag=OnetimeFlag.is_a_walk(one_time_flag),
             )
 
